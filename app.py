ee0733eea498648f8a447e8b3174b717874343d4
plefebvre1 <peter.c.lefebvre@gmail.com>
2022-02-20 20:56:13 -0500

    Included updated visualizations for all pairs, and updated main app to work with them

--- app.py
+++ app.py
@@ -8,25 +8,24 @@ import questionary
 from Functions.data_cleaning import clean_data
 from Functions.indicator import indicator
 
+#get environment variable and create API key variable
 load_dotenv()
 fred_api_key = os.getenv('FRED_API_KEY')
 
+#get user input
 category_name = questionary.select('Which currency pair do you want to evaluate?', choices = ['USD/EUR', 'JPY/USD', 'USD/GBP', 'CAD/USD', 'MXN/USD']).ask()
 print('Now analyzing...')
+
+#map user input to category id to use with API
 categories = {'USD/EUR' : 'DEXUSEU',
-              'Yen/USD' : 'DEXJPUS',
+              'JPY/USD' : 'DEXJPUS',
               'USD/GBP' : 'DEXUSUK',
               'CAD/USD' : 'DEXCAUS',
               'MXN/USD' : 'DEXMXUS'
              }
 category_id = categories[category_name]
 
-if category_name == 'USD/EUR':
-    filepath = 'Graphs_and_things-vpl.ipynb'
-    print("To see your results copy and paste the following command in your terminal:")
-    print(f'voila {filepath}')
-
-
+#get results from functions
 spot_rate, results = clean_data(fred_api_key,category_id)
     
 #print results
@@ -44,10 +43,27 @@ print(f'EMA50 = ${results[9]: .4f}')
 print(f'EMA100 = ${results[10]: .4f}')
 print(f'EMA200 = ${results[11]: .4f}')
 
+#get overall recommendation
 total = 0
 
 for avg in results: 
-    
     indicator_result = indicator(spot_rate,avg)
     total = total + indicator_result
-print(f'{total/12*100: .2f}%')
+print(f'The buy strength is {total/12*100: .2f}%')
+
+#give user voila path to visualize results from CLI
+print("To see your results copy and paste the following command in your terminal:")
+if category_name == 'USD/EUR':
+    filepath = 'USD_EUR_Charts.ipynb'
+elif category_name == 'JPY/USD':
+    filepath = 'JPY_USD_Charts.ipynb'
+elif category_name == 'USD/GBP':
+    filepath = 'USD_GBP_Charts.ipynb'
+elif category_name == 'CAD/USD':
+    filepath = 'CAD_USD_Charts.ipynb'
+else:
+    filepath = 'MXN_USD_Charts.ipynb'
+
+    
+print(f'voila {filepath}')
+    
