8e8083410bd73fb75176248775d5432cee3ea430
plefebvre1 <peter.c.lefebvre@gmail.com>
2022-02-20 20:57:36 -0500

    Removed unnecessary files

--- python2.py
+++
@@ -1,8 +0,0 @@
-import questionary
-
-currency_pair = questionary.select('Which currency pair do you want to evaluate?', choices = ['USD/EUR']).ask()
-print('Now analyzing...')
-if currency_pair == 'USD/EUR':
-    filepath = 'Graphs_and_things-vpl.ipynb'
-    print("To see your results copy and paste the following command in your terminal:")
-    print(f'voila {filepath}')
